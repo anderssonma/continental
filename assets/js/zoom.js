608b6dbc24326d409a4422f17d208dff5df30076
Marcus Andersson <marcus@marcus-andersson.com>
2014-03-21 13:37:54 +0100

    Foundation for medium-style Gallery

---
+++ assets/js/zoom.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+
+	var isWrapped = false;
+	$('.gallery img').on('click', function() {
+		if (isWrapped) {
+			//$(this).unwrap('<div class="zoom-img-wrap"></div>');
+			isWrapped = false;
+			
+			$(this).animate({ textIndent: 0 }, {
+			    step: function(now,fx) {
+			    	var integer = (now / 100).toString();
+			    	integer = integer.substring(integer.indexOf(".") + 1);
+			    	integer = parseFloat('1.' + integer, 10);
+			    	
+			    	if (integer === 1.1) {
+			    		integer = 2;
+			    	} else {
+			    		integer.toFixed(6);
+			    	}
+			        //$(this).css('-webkit-transform',"translate3d(0px, " + now + "px, 0px)");
+			        $(this).css('-webkit-transform', 'scale(' + integer + ')');
+			    },
+			    duration: 350
+			}, 'ease-out', function() {
+			  $(this).parent().removeClass('front');
+			});
+		} else {
+			//$(this).wrap('<div class="zoom-img-wrap"></div>');
+			isWrapped = true;
+			$(this).parent().addClass('front');
+			$(this).animate({ textIndent: 100 }, {
+			    step: function(now,fx) {
+			    	var integer = (now / 100).toString();
+			    	integer = integer.substring(integer.indexOf(".") + 1);
+			    	integer = parseFloat('1.' + integer, 10);
+			    	
+			    	if (integer === 1.1) {
+			    		integer = 2;
+			    	} else {
+			    		integer.toFixed(6);
+			    	}
+			        //$(this).css('-webkit-transform',"translate3d(0px, " + now + "px, 0px)");
+			        $(this).css('-webkit-transform', 'scale(' + integer + ')');
+			    },
+			    duration: 350
+			},'ease-out');
+			
+		}
+	});
+
+
+
+
+});
+
