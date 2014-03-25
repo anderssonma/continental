d3b9c9f328c2e9ddf8786b9e91ff990895b678d4
Marcus Andersson <marcus@marcus-andersson.com>
2014-03-25 17:19:53 +0100

    Improves Gallery

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
 
 	var isWrapped = false;
 	$('.gallery img').on('click', function() {
+		var direction = $(this).data('align');
 		if (isWrapped) {
 			//$(this).unwrap('<div class="zoom-img-wrap"></div>');
 			isWrapped = false;
-			
+
+			$('.gallery').removeClass('dim');
 			$(this).animate({ textIndent: 0 }, {
 			    step: function(now,fx) {
 			    	var integer = (now / 100).toString();
@@ -14,29 +16,43 @@ $(document).ready(function() {
 			    	
 			    	if (integer === 1.1) {
 			    		integer = 2;
+			    		$(this).parent().removeClass('front');
+			    	}
+
+			    	var nextMod = (now * 1.25);
+			    	if (direction === 'right') {
+			    		nextMod = -nextMod;
 			    	}
 			        //$(this).css('-webkit-transform',"translate3d(0px, " + now + "px, 0px)");
-			        $(this).css('-webkit-transform', 'scale(' + integer + ')');
+						
+			        $(this).css('-webkit-transform', 'scale(' + integer + ') translateX(' + nextMod + 'px) translateY(' +  (now * 0.9) + 'px)');
 			    },
 			    duration: 350
 			}, function() {
-			  $(this).parent().removeClass('front');
+				console.log('H');
+			  //$(this).parent().removeClass('front');
 			});
 		} else {
 			//$(this).wrap('<div class="zoom-img-wrap"></div>');
 			isWrapped = true;
 			$(this).parent().addClass('front');
+			$('.gallery').addClass('dim');
 			$(this).animate({ textIndent: 100 }, {
 			    step: function(now,fx) {
 			    	var integer = (now / 100).toString();
 			    	integer = integer.substring(integer.indexOf(".") + 1);
 			    	integer = parseFloat('1.' + integer, 10);
-			    	
+			    	console.log(now);
 			    	if (integer === 1.1) {
 			    		integer = 2;
 			    	}
+
+			    	var nextMod = (now * 1.25);
+			    	if (direction === 'right') {
+			    		nextMod = -nextMod;
+			    	}
 			        //$(this).css('-webkit-transform',"translate3d(0px, " + now + "px, 0px)");
-			        $(this).css('-webkit-transform', 'scale(' + integer + ')');
+			        $(this).css('-webkit-transform', 'scale(' + integer + ') translateX(' + nextMod + 'px) translateY(' +  (now * 0.9) + 'px)');
 			    },
 			    duration: 350
 			});
