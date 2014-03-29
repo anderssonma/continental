1164f7edaeef224ad7c949cdb54d467469032454
Marcus Andersson <marcus@marcus-andersson.com>
2014-03-29 15:05:19 +0100

    PANO: Clean up html, convert to sass & optimize JS

--- assets/js/pano.js
+++ assets/js/pano.js
@@ -1,16 +1,15 @@
 var Pano = {
-	isHidden: false,
-	textTimer: null
+	isHidden: false
 };
 
 $(document).ready(function (){
 
 	var panoReady = function(pano) {
 		var krpano = pano;
-		console.log(pano);
+
 		window.setTimeout(function() {
-			//krpano.set("autorotate.enabled", true);
-			
+			// SETUP ALL BINDINGS
+			// GIVE KRPANO A COUPLE OF MS TO FINISH
 
 			// ZOOM IN/OUT BTNS
 			$('#pano-zoom-in').on('mousedown', function() {
@@ -48,63 +47,37 @@ $(document).ready(function (){
 				krpano.set('vlookat_moveforce',0);
 			});
 
-		}, 100);
+		}, 250);
 	}
+
 	embedpano({swf:"assets/js/vendor/pano_sphere.swf", xml:"assets/js/vendor/pano_sphere.xml", target:"pano", html5:"prefer", passQueryParameters:true, wmode:"opaque", mwheel: false, onready:panoReady});
 	
-
-	
-
-	//console.log(krpano.get('view.fov'));
-	/*
-	window.setTimeout(function() {
-		window.scrollTo(0,1);
-	}, 0);
-	*/
-
-
 	// GET SUPPORT STATUS
 	Pano.support = {};
 	Pano.support.touch = Modernizr.touch;
 	Pano.support.webgl = Modernizr.webgl;
 	Pano.support.csstra = Modernizr.csstransitions;
 
+	// CACHE SELECTORS
+	var pano = $('#pano');
+	var panoWrap = $('#pano-wrap');
 
-	$('.modal-overlay').on('click', function() {
-		$('#pano-wrap').removeClass('form-active');
-	});
+	var introBox = $('#intro')
 
-	/*
-	var activeCache;
-	$('.filter').on('click', function() {
-		$('body').removeClass();
-		$('body').addClass($(this).data('effect'));
-		if(typeof activeCache !== 'undefined'){
-			activeCache.removeClass('active');
-		}
-		activeCache = $(this);
-		$(this).addClass('active');
-	});
-
-	$('#reset-filter').on('click', function() {
-		$('body').removeClass();
-		if(typeof activeCache !== 'undefined'){
-			activeCache.removeClass('active');
-		}
-	});
-*/
-
+	var btnIntroShow = $('#btn-show-intro');
+	var btnIntroHide = $('#btn-hide-intro');
+	var btnScrollDown = $('#btn-scroll-down');
 
 	var introHide = function() {
 		if (Pano.support.csstra) { // IF BETTER IE9 AND BELOW
-			$('#pano-wrap').addClass('panning hide');
+			$(panoWrap).addClass('panning hide');
 		} else {
-			$('#pano-wrap').addClass('panning');
+			$(panoWrap).addClass('panning');
 			window.setTimeout(function() {
-			$('#intro').animate({opacity: 0, marginTop: '-985px'}, 500);
-			$('#scroll-down').animate({bottom: '-100px'}, 250);
+			$(introBox).animate({opacity: 0, marginTop: '-985px'}, 500);
+			$(btnScrollDown).animate({bottom: '-100px'}, 250);
 				window.setTimeout(function() {
-					$('#expand').animate({top: 0}, 250);
+					$(btnIntroShow).animate({top: 0}, 250);
 				}, 350);
 			}, 0);
 		}
@@ -113,84 +86,43 @@ $(document).ready(function (){
 
 	var introShow = function() {
 		if (Pano.support.csstra) {
-			$('#pano-wrap').removeClass('panning hide');
+			$(panoWrap).removeClass('panning hide');
 		} else {
-			$('#pano-wrap').removeClass('panning');
+			$(panoWrap).removeClass('panning');
 			window.setTimeout(function() {
-				$('#expand').animate({top: '-50px'}, 250);
+				$(btnIntroShow).animate({top: '-50px'}, 250);
 				window.setTimeout(function() {
-					$('#intro').animate({opacity: 1, marginTop: '-185px'}, 250);
-					$('#scroll-down').animate({bottom: '20px'}, 250);
+					$(introBox).animate({opacity: 1, marginTop: '-185px'}, 250);
+					$(btnScrollDown).animate({bottom: '20px'}, 250);
 				}, 350);
 			}, 0);
 		}
 		Pano.isHidden = false;
 	};
 
-	$('#expand').on('click', function() {
+	// SHOW & HIDE INTRO
+	$(btnIntroShow).on('click', function() {
 		if (Pano.isHidden) {
-			window.clearTimeout(Pano.textTimer);
 			introShow();
 		}
 	});
-
-	$('#close-overlay').on('click', function() {
-		$('body').removeClass('form-active');
-	});
-
-	$('#scroll-down').on('click', function() {
-		$('#first-screen').trigger('click');
-	});
-
-	$('#trigger-form').on('click', function() {
+	$(btnIntroHide).on('click', function() {
 		if (!Pano.isHidden) {
 			introHide();
 		}
 	});
+	// SCROLL TO FIRST SECTION
+	$(btnScrollDown).on('click', function() {
+		$('#first-screen').trigger('click');
+	});
 
-	/*
-	if (!Pano.support.touch) { // NEED SOMETHING BETTER TO DISTINGUISH
-		$('#pano').on('keydown', function(e) {
-			if (!Pano.isHidden) { // ONLY HIDE ON ARROW KEYS DOWN
-				if (e.keyCode === 37 || e.keyCode === 38 || e.keyCode === 39 || e.keyCode === 40) {
-					introHide();
-				}
-			}
-		});
-		$('#pano').on('mousedown', function(e) {
-			if (!Pano.isHidden) {
-				introHide();
-			}
-		});
+	// ADD DIFFERENT CURSORS DEPENDING ON INTERACTIION
+	$(pano).on('mousedown', function() {
+		$(pano).addClass('moving');
+	}).on('mouseup', function() {
+		$(pano).removeClass('moving');
+	});
 
-	*/
-		$('#pano').on('mousedown', function() {
-			$('#pano').addClass('moving');
-			if (Pano.isHidden) {}
-		});
-
-		$('#pano').on('mouseup', function() {
-			$('#pano').removeClass('moving');
-			if (Pano.isHidden) {}
-		});
-
-	/*
-
-	} else {
-		$('#pano').on('touchstart', function(e) {
-			if (!Pano.isHidden) {
-				introHide();
-			}
-		});
-		$('#pano').on('touchend', function() {
-			if (Pano.isHidden) {
-				window.clearTimeout(Pano.textTimer); // CLEAR PREVIOUS TIMEOUT IF IT'S STILL IN PROGRESS
-				Pano.textTimer = window.setTimeout(function() {
-					introShow();
-				}, 4000);
-			}
-		});
-	}
-	*/
+	// if (!Pano.support.touch) {} FOR MOBILE/DESKTOP FUNCTIONALITY
 
 });
