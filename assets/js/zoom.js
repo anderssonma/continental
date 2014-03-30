2ca05b9a9fcc15f0945e64c04f7f0c1b197bc629
Marcus Andersson <marcus@marcus-andersson.com>
2014-03-30 22:48:02 +0200

    Adds gallery function, new images

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,5 +1,86 @@
 $(document).ready(function() {
 
+	var activityIndicatorOn = function() {
+		$('<div id="imagelightbox-loading"><div></div></div>').appendTo('body');
+	};
+	
+	var activityIndicatorOff = function() {
+		$('#imagelightbox-loading').remove();
+	};
+
+	var overlayOn = function() {
+		$('<div id="imagelightbox-overlay"></div>').appendTo('body');
+	};
+
+	var overlayOff = function() {
+		$( '#imagelightbox-overlay' ).remove();
+	};
+
+	var closeButtonOn = function(instance) {
+		$('<a href="#" id="imagelightbox-close">Close</a>').appendTo('body').on( 'click', function() {$(this).remove(); instance.quitImageLightbox(); return false;});
+	};
+	
+	var closeButtonOff = function() {
+		$('#imagelightbox-close').remove();
+	};
+
+	var captionOn = function() {
+		var description = $('a[href="' + $('#imagelightbox').attr('src') + '"] img').attr('alt');
+		if (description.length > 0) {
+			$('<div id="imagelightbox-caption">' + description + '</div>').appendTo('body');
+		}
+	};
+	
+	var captionOff = function() {
+		$('#imagelightbox-caption').remove();
+	};
+
+	var navigationOn = function(instance, selector) {
+		var images = $(selector);
+		if (images.length) {
+			var nav = $('<div id="imagelightbox-nav"></div>');
+			for(var i = 0; i < images.length; i++)
+				nav.append( '<a href="#"></a>');
+
+			nav.appendTo('body');
+			nav.on( 'click touchend', function(){ return false; });
+
+			var navItems = nav.find('a');
+			navItems.on('click touchend', function() {
+				var $this = $( this );
+				if (images.eq($this.index()).attr('href') != $('#imagelightbox').attr('src')) {
+					instance.switchImageLightbox($this.index());
+				}
+				navItems.removeClass('active');
+				navItems.eq($this.index()).addClass('active');
+
+				return false;
+			}).on('touchend', function() { return false; });
+		}
+	};
+			
+	var navigationUpdate = function(selector) {
+		var items = $('#imagelightbox-nav a');
+		items.removeClass('active');
+		items.eq($(selector).filter('[href="' + $('#imagelightbox').attr('src') + '"]').index(selector)).addClass('active');
+	};
+	
+	var navigationOff = function() {
+		$('#imagelightbox-nav').remove();
+	};
+
+	var selectorF = '.images a';
+	var instanceF = $(selectorF).imageLightbox({
+		onStart:			function() {overlayOn(); closeButtonOn( instanceF ); navigationOn( instanceF, selectorF );},
+		onEnd:				function() {overlayOff(); captionOff(); closeButtonOff(); navigationOff(); activityIndicatorOff();},
+		onLoadStart:	function() {captionOff(); activityIndicatorOn();},
+		onLoadEnd:		function() {captionOn(); navigationUpdate( selectorF ); activityIndicatorOff();}
+	});
+});
+
+/*
+$(document).ready(function() {
+
 	var prefix = Modernizr.prefixed('transform');
 	prefix = prefix.replace(/([A-Z])/g, function(prefix,m1){ return '-' + m1.toLowerCase(); }).replace(/^ms-/,'-ms-');
 	var isWrapped = false;
@@ -65,4 +146,4 @@ $(document).ready(function() {
 
 
 });
-
+*/
